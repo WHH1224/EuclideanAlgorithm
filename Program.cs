8fedfc991c215aef8c5f44a50adf9f66e9340916
WHH1224 <hsinhuiww@gmail.com>
2024-11-26 22:04:18 +0800

    Fitst commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace 輾轉相除法
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] intput;
+            while ((intput = Console.ReadLine().Split()) != null)
+            {
+
+                int a = int.Parse(intput[0]);
+                int b = int.Parse(intput[1]);
+                int ans = 0;
+
+                for (int i = 0; i < a + b; i++)
+                {
+                    if (a > b)
+                    {
+                        a = a % b;
+                        if (a == 0)
+                        {
+                            ans = b;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        b = b % a;
+                        if (b == 0)
+                        {
+                            ans = a;
+                            break;
+                        }
+                    }
+                }
+                Console.WriteLine(ans);
+            }
+           
+        }
+    }
+}
